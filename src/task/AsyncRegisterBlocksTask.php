004242f10a9c23efe342ab36c2550f9d1445f9c6
Nyrok <47859726+Nyrok@users.noreply.github.com>
2023-05-08 09:22:34 +0200

    Fix error with AsyncRegisterBlocksTask
    Error: "Closures with non-thread-safe $this cannot be made thread-safe"

--- src/task/AsyncRegisterBlocksTask.php
+++ src/task/AsyncRegisterBlocksTask.php
@@ -7,33 +7,43 @@ use customiesdevs\customies\block\CustomiesBlockFactory;
 use customiesdevs\customies\util\Cache;
 use pocketmine\block\Block;
 use pocketmine\scheduler\AsyncTask;
-use ThreadedArray;
+use pocketmine\thread\NonThreadSafeValue;
 
 final class AsyncRegisterBlocksTask extends AsyncTask {
 
-	private ThreadedArray $blockFuncs;
-	private ThreadedArray $objectToState;
-	private ThreadedArray $stateToObject;
+    /**
+     * @var NonThreadSafeValue[]
+     */
+	private array $blockFuncs;
+    /**
+     * @var NonThreadSafeValue[]
+     */
+	private array $objectToState;
+    /**
+     * @var NonThreadSafeValue[]
+     */
+	private array $stateToObject;
 
 	/**
 	 * @param Closure[] $blockFuncs
 	 * @phpstan-param array<string, Closure(int): Block> $blockFuncs
 	 */
 	public function __construct(private string $cachePath, array $blockFuncs) {
-		$this->blockFuncs = new ThreadedArray();
-		$this->objectToState = new ThreadedArray();
-		$this->stateToObject = new ThreadedArray();
+		$this->blockFuncs = [];
+		$this->objectToState = [];
+		$this->stateToObject = [];
 
 		foreach($blockFuncs as $identifier => [$blockFunc, $objectToState, $stateToObject]){
-			$this->blockFuncs[$identifier] = $blockFunc;
-			$this->objectToState[$identifier] = $objectToState;
-			$this->stateToObject[$identifier] = $stateToObject;
+			$this->blockFuncs[$identifier] = new NonThreadSafeValue($blockFunc);
+			$this->objectToState[$identifier] = new NonThreadSafeValue($objectToState);
+			$this->stateToObject[$identifier] = new NonThreadSafeValue($stateToObject);
 		}
 	}
 
 	public function onRun(): void {
 		Cache::setInstance(new Cache($this->cachePath));
 		foreach($this->blockFuncs as $identifier => $blockFunc){
+            $blockFunc = $blockFunc->deserialize();
 			// We do not care about the model or creative inventory data in other threads since it is unused outside of
 			// the main thread.
 			CustomiesBlockFactory::getInstance()->registerBlock($blockFunc, $identifier, objectToState: $this->objectToState[$identifier], stateToObject: $this->stateToObject[$identifier]);
